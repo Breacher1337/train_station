14f6a5e4be21789f694e4e1153039f0a54a39e02
Elaijah <elaijahangelo@gmail.com>
2024-10-30 23:32:46 +0800

    major adjustments, removed redundancy code in station switxh

--- main.c
+++ main.c
@@ -32,20 +32,22 @@ int getStartingLine() {
 
 int getCardType() {
 	int card_type = 0;
-	int has_card_type = 0;
-	while (!has_card_type) {
-	printf("Please select your type of card. \n1. Regular\n2. Student\n3. PWD/Senior\n4. Single Journey Ticket\nCard type: ");
-	scanf("%d", &card_type);
+    while (1) {
+        printf("Please select your type of card.\n");
+        printf("1. Regular\n2. Student\n3. PWD/Senior\n4. Single Journey Ticket\nCard type: ");
+        
+        if (scanf("%d", &card_type) != 1) {
+            while (getchar() != '\n'); 
+            printf("Error. Please enter a valid number (1, 2, 3, or 4).\n");
+            continue; 
+        }
 
-	if (card_type == 1 || card_type == 2 || card_type ==  3 || card_type == 4 )
-	{
-		has_card_type = 1;
-	} else {
-		printf("Error. Please try again.\n");
-		return getCardType();
-	}
-	}
-	return card_type;
+        if (card_type >= 1 && card_type <= 4) {
+            return card_type;  
+        } else {
+            printf("Error. Please try again.\n");
+        }
+    }
 	}
 
 float getDiscountMultiplier(int card_type) {
@@ -129,92 +131,68 @@ int calculateFareSJ(int starting_line, int starting_station, int destination_sta
 	return fare;
 }
 	
-void checkStationSwitch(int *starting_line, int *starting_station) {
-    if (*starting_line == 1 && *starting_station == 2) {
-        printf("You can switch to MRT3 Taft Ave.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 3;
-            *starting_station = 13;
-        }
-    } else if (*starting_line == 1 && *starting_station == 11) {
-        printf("You can switch to LRT2 Doroteo Jose.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 2;
-            *starting_station = 1;
-        }
-    } else if (*starting_line == 2 && *starting_station == 1) {
-        printf("You can switch to LRT1 Doroteo Jose.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 1;
-            *starting_station = 11;
-        }
-    } else if (*starting_line == 2 && *starting_station == 8) {
-        printf("You can switch to MRT3 Cubao.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 3;
-            *starting_station = 4;
-        }
-    } else if (*starting_line == 3 && *starting_station == 13) {
-        printf("You can switch to LRT1 EDSA.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 1;
-            *starting_station = 2;
-        }
-    } else if (*starting_line == 3 && *starting_station == 4) {
-        printf("You can switch to LRT2 Cubao.\n");
-        printf("Do you want to switch lines? (Y/N)\n");
-        char choice = getch();
-        if (choice == 'Y' || choice == 'y') {
-            *starting_line = 2;
-            *starting_station = 8;
-        }
-    } else if (*starting_line == 3 && *starting_station == 1) {
-		printf("You can switch to LRT1 Fernando Poe Jr.\n");
-		printf("Do you want to switch lines? (Y/N)\n");
-		char choice = getch();
-		if (choice == 'Y' || choice == 'y') {
-			*starting_line = 1;
-			*starting_station = 20;
-		}
-	} else if (*starting_line == 1 && *starting_station == 20) {
-		printf("You can switch to MRT3 North Ave.\n");
-		printf("Do you want to switch lines? (Y/N)\n");
-		char choice = getch();
-		if (choice == 'Y' || choice == 'y') {
-			*starting_line = 3;
-			*starting_station = 1;
-		}
-	}
+typedef struct {
+    int line;
+    int station;
+    int new_line;
+    int new_station;
+    const char* switch_message;
+} StationSwitch;
+
+int promptSwitchConfirmation() {
+    char choice;
+    do {
+        printf("Do you want to switch lines? (Y/N): ");
+        while (getchar() != '\n');  // Clear the buffer
+        choice = getchar();
+    } while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
+    
+    return (choice == 'Y' || choice == 'y');
 }
 
-int getBalance() {
+void checkStationSwitch(int *starting_line, int *starting_station) {
+    StationSwitch switches[] = {
+        {1, 2, 3, 13, "You can switch to MRT3 Taft Ave.\n"},
+        {1, 11, 2, 1, "You can switch to LRT2 Doroteo Jose.\n"},
+        {2, 1, 1, 11, "You can switch to LRT1 Doroteo Jose.\n"},
+        {2, 8, 3, 4, "You can switch to MRT3 Cubao.\n"},
+        {3, 13, 1, 2, "You can switch to LRT1 EDSA.\n"},
+        {3, 4, 2, 8, "You can switch to LRT2 Cubao.\n"},
+        {3, 1, 1, 20, "You can switch to LRT1 Fernando Poe Jr.\n"},
+        {1, 20, 3, 1, "You can switch to MRT3 North Ave.\n"}
+    };
+
+    for (int i = 0; i < sizeof(switches) / sizeof(switches[0]); i++) {
+        if (*starting_line == switches[i].line && *starting_station == switches[i].station) {
+            printf("%s", switches[i].switch_message);
+            if (promptSwitchConfirmation()) {
+                *starting_line = switches[i].new_line;
+                *starting_station = switches[i].new_station;
+            }
+            break;
+        }
+    }
+}
+
+int getBalance(int card_type) {
 	int balance = 0;
 	
-	printf("Please enter your balance: ");
-	scanf("%d", &balance);
+    while (1) {
+        printf("Please enter your balance: ");
 
-	if (balance == 4) {
-		if (balance < 15) {
-			printf("You don't have enough balance to ride.\n");
-			exit(0);
-		}
-	} else {
-		if (balance < 20) {
-			printf("You don't have enough balance to ride.\n");
-			exit(0);
-		}
-	}
-	return balance;
+        if (scanf("%d", &balance) != 1) {
+            while (getchar() != '\n');  
+            printf("Error. Please enter a valid number.\n");
+            continue; 
+        }
+
+        if ((card_type == 4 && balance < 15) || (card_type != 4 && balance < 20)) {
+            printf("You don't have enough balance to ride.\n");
+            exit(0); 
+        }
+
+        return balance;
+    }
 }
 
 int calculateBalance(int balance, int total_fare) {
@@ -222,81 +200,120 @@ int calculateBalance(int balance, int total_fare) {
 	return new_balance;
 }
 
-int main()
-{
-	int starting_line = getStartingLine();
-	int card_type = getCardType();
-	float discount_multiplier = getDiscountMultiplier(card_type);
-	int balance = getBalance();
-	int station_switch_first_check = 0;
-	int starting_station, destination_station;
-
-	printf("\nWhich station are you starting from?\n");
-	displayStations(starting_line);
-	scanf("%d", &starting_station);
-	int is_finished = 0;
-	int total_fare = 0;
-
-	while (!is_finished) {
-
-		//checker to make sure that the station switch happens after the first station
-		if (station_switch_first_check == 0)
-		{
-			station_switch_first_check = 1;
-		}
-		else
-		{
-			checkStationSwitch(&starting_line, &starting_station);
-		}
-
-		switch (starting_line)
-		{
+int isValidStation(int starting_line, int station) {
+	switch (starting_line) {
 		case 1:
-			printf("You are currently at %s.\n", lrt1_stations[starting_station - 1]);
-			break;
+			return station >= 0 && station <= LRT1_NUM_STATIONS;
 		case 2:
-			printf("You are currently at %s.\n", lrt2_stations[starting_station - 1]);
-			break;
+			return station >= 0 && station <= LRT2_NUM_STATIONS;
 		case 3:
-			printf("You are currently at %s.\n", mrt3_stations[starting_station - 1]);
-			break;
+			return station >= 0 && station <= MRT3_NUM_STATIONS;
 		default:
-			printf("Error. Please try again.\n");
-		}
+			return 0;
+	}
+}
 
-		printf("Which station are you going to?\n");
-		displayStations(starting_line);
-		scanf("%d", &destination_station);
+int main() {
+    int starting_line = getStartingLine();
+    int card_type = getCardType();
+    float discount_multiplier = getDiscountMultiplier(card_type);
+    int balance = getBalance(card_type);
+    int station_switch_first_check = 0;
+    int starting_station = 0, destination_station = 0;
+    int is_finished = 0;
+    int total_fare = 0;
+	char continue_ride;
 
-		if (destination_station == 0)
-		{
-			printf("Thank you for using Elaijah's Station Simulator!\n");
-			exit(0);
-		}
-		
-		if (starting_station == destination_station) {
-			printf("You are already at your destination.\n");
-		} else {
-			int fare = calculateFare(starting_line, starting_station, destination_station);
-			total_fare += fare * discount_multiplier;
-			printf("Your fare is: P%d\n", total_fare);
+    printf("\nWhich station are you starting from?\n");
+    displayStations(starting_line);
 
-			balance = calculateBalance(balance, total_fare);
-			printf("Your balance is: P%d\n", balance);
-		}
-		
-		printf("Do you want to ride again? (Y/N)\n\n\n");
-		char choice = getch();
-		if (choice == 'N' || choice == 'n') {
+    while (1) {
+        printf("Enter starting station number: ");
+        if (scanf("%d", &starting_station) != 1) {
+            while (getchar() != '\n');  // Clear invalid input
+            printf("Error. Please enter a valid station number.\n");
+            continue;
+        }
+        if (isValidStation(starting_line, starting_station)) break; 
+        else printf("Invalid station number. Please try again.\n");
+    }
+
+	system("cls");
+
+    while (!is_finished) {
+        if (station_switch_first_check == 0) {
+            station_switch_first_check = 1;
+        } else {
+            checkStationSwitch(&starting_line, &starting_station);
+        }
+
+        switch (starting_line) {
+            case 1:
+                printf("You are currently at %s.\n", lrt1_stations[starting_station - 1]);
+                break;
+            case 2:
+                printf("You are currently at %s.\n", lrt2_stations[starting_station - 1]);
+                break;
+            case 3:
+                printf("You are currently at %s.\n", mrt3_stations[starting_station - 1]);
+                break;
+            default:
+                printf("Error. Invalid line number.\n");
+                continue;
+        }
+
+        printf("Which station are you going to?\n");
+        displayStations(starting_line);
+        while (1) {
+            printf("Enter destination station number (or 0 to exit): ");
+            if (scanf("%d", &destination_station) != 1) {
+                while (getchar() != '\n');  
+                printf("Error. Please enter a valid station number.\n");
+                continue;
+            }
+            if (destination_station == 0) {
+                printf("Thank you for using Elaijah's Station Simulator!\n");
+                exit(0);
+            }
+            if (isValidStation(starting_line, destination_station)) break;  
+            else printf("Invalid station number. Please try again.\n");
+        }
+
+        if (starting_station == destination_station) {
+            printf("You are already at your destination.\n");
+        } else {
+            int fare = calculateFare(starting_line, starting_station, destination_station);
+            total_fare += fare * discount_multiplier;
+			printf("Your fare is: P%d\n", fare);
+            printf("Your total fare is: P%d\n", total_fare);
+
+            balance = calculateBalance(balance, total_fare);
+            printf("Your balance is: P%d\n", balance);
+
+            if (balance < 0) {
+                printf("Insufficient balance for further rides.\n");
+                exit(0);
+            }
+        }
+
+		do {
+			printf("Do you want to ride again? (Y/N): ");
+			while (getchar() != '\n');  // clear buffer, removes any extra characters
+			continue_ride = getchar();
+		} while (continue_ride != 'Y' && continue_ride != 'y' && continue_ride != 'N' && continue_ride != 'n');
+
+		if (continue_ride == 'N' || continue_ride == 'n') {
 			is_finished = 1;
 		}
+
+		system("cls");
 
 		starting_station = destination_station;
 	}
 
-	printf("Your total fare is: P%d\n", total_fare);
-	printf("Your balance is: P%d\n", balance);
-	printf("Thank you for using Elaijah's Station Simulator!\n");
+    printf("Your total fare is: P%d\n", total_fare);
+    printf("Your balance is: P%d\n", balance);
+    printf("Thank you for using Elaijah's Station Simulator!\n");
 
-	return 0;
+    return 0;
 }
